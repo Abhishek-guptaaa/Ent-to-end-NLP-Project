2406394732f9c54f175d3c642f9acf0889406f4b
Abhishek Gupta <Abhishek18895gupta@gmail.com>
2024-06-19 12:08:37 +0530

    install requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Abhishke",
+    author_email="Abhishek18895gupta@gmail.com",
+    packages=find_packages(),
+)
